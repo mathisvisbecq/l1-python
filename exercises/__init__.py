18bbb01187a7c6743be73fbfee84b1ee6576097e
Mathis VISBECQ <mathis.visbecq@ens.uvsq.fr>
2021-11-09 15:39:43 +0100

    l

--- exercises/__init__.py
+++ exercises/__init__.py
@@ -2,7 +2,8 @@
 
 def tempsEnSeconde(temps):
     """ Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde."""
-    pass
+    s = temps[0] * 86400 + temps[1] * 3600 + temps[2] * 60 + temps[3]
+    return s
 
 temps = (3,23,1,34)
 print(type(temps))
@@ -10,7 +11,19 @@ print(tempsEnSeconde(temps))
 
 def secondeEnTemps(seconde):
     """Renvoie le temps (jour, heure, minute, seconde) qui correspond au nombre de seconde passé en argument"""
-    pass
+    j = 342094 // 86400
+    hs = 342094 % 3600
+    h = hs - j * 24
+    m = hs // 60
+    s = hs % 60
+    print(j, h, m, s)
+    return
+    
+
     
 temps = secondeEnTemps(100000)
 print(temps[0], "jour", temps[1], "heure", temps[2], "minute", temps[3], "seconde")
+
+
+
+
